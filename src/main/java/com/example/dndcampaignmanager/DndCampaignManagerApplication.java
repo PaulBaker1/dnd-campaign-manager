bf99e2b79ee3318199ef5b649ea4d276bc4fb3df
PaulBaker1 <p.b.kopietz@gmail.com>
2025-02-06 16:47:12 +0100

    #1 First Issue

---
+++ src/main/java/com/example/dndcampaignmanager/DndCampaignManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dndcampaignmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndCampaignManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndCampaignManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dndcampaignmanager/DndCampaignManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dndcampaignmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndCampaignManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
